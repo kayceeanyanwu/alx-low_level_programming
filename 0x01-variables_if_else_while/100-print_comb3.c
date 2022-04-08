6e15b7bbfd801acffc415a8eb2fba47d993aeab9
kayceeanyanwu <kayceeanyanwu@gmail.com>
2022-04-08 13:31:38 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -23,11 +23,9 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
